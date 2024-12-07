cc1f340bbb6858d4b90aa6fafbc87c5c9cb5927d
Rahim Abbas  Sindhu <irahimsindhu@gmail.com>
2024-12-07 14:12:06 +0500

    Update index.js by hiding API Keys

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const PORT = 8000;
 const stripe = require("stripe")(
-  "sk_test_51OD8ueKdpJpi62pjsBjYVTZXooq6uNmb0dAYdoU0YXBDiJ0dktK94sk9enCVQmr1PHm3kVp6OYc7Vqn4SVPPpDqq00Jgc1Bpai"
+  "YOUR-STRIPE-API-SECRET-KEY"
 );
 const app = express();
 app.use(cors());
@@ -32,7 +32,7 @@ app.post("/payment-sheet", async (req, res) => {
     ephemeralKey: ephemeralKey.secret, // Correct spelling
     customer: customer.id,
     publishableKey:
-      "pk_test_51OD8ueKdpJpi62pjGycuvfFpLLKfnoFOXk6Qnfl3Bb5tF1vw5RbQ7qtjYeE6lS1tGXadxVXaVeS0DHEbSiXl4QRj00frWRlDm7",
+      "YOUR-STRIPE-API-PUBLIC-KEY",
   });
 });
 
